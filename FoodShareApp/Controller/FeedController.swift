8042f2f8f36e0559d898f4b41a6b7be6b50b6989
kazuma1209 <sakama.skt.1209@icloud.com>
2021-05-01 23:00:47 +0900

    1day

---
+++ FoodShareApp/Controller/FeedController.swift
@@ -0,0 +1,18 @@
+//
+//  FeedController.swift
+//  FoodShareApp
+//
+//  Created by 坂田一真 on 2021/05/01.
+//
+
+import UIKit
+
+class FeedController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red: 255/255, green: 255/255, blue: 240/255, alpha: 1.0)
+        
+    }
+}
